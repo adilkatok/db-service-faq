51f54dceceb770ef62692248b298f4dc2f546be7
adilkatok <tokushev.adil@gmail.com>
2019-10-08 14:21:57 +0600

    rest controllers for answers and questions and also customs exception for 404

--- src/main/java/kz/adilka/dbservice/model/Answer.java
+++ src/main/java/kz/adilka/dbservice/model/Answer.java
@@ -27,4 +27,27 @@ public class Answer extends JPAAuditingModel {
     @JsonIgnore
     private Question question;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
 }
