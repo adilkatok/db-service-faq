502a0c8b921f8a5cd6689f767b06b15b336a4ca7
adilkatok <tokushev.adil@gmail.com>
2019-10-08 13:02:17 +0600

    Initial commit - initialize spring and database settings

---
+++ src/main/java/kz/adilka/dbservice/DbServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.adilka.dbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbServiceApplication.class, args);
+	}
+
+}
